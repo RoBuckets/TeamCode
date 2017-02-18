2a349588051b63a7813f8a73cc110902889c3c77
RoBuckets <nmhsrobuckets@gmail.com>
2017-02-17 16:39:33 -0800

    Update robuckets_autonomous_regionals.java

--- regionals/robuckets_autonomous_regionals.java
+++ regionals/robuckets_autonomous_regionals.java
@@ -16,10 +16,10 @@ public class robuckets_autonomous_regionals extends LinearOpMode {
     public DcMotor leftBack;
 
     public DcMotor Launch1;
-    public DcMotor Launch2
+    public DcMotor Launch2;
     public DcMotor collector;
     public Servo launchServo;
-    
+
     public int active = 0;
 
     ColorSensor color_sensor;
@@ -32,12 +32,12 @@ public class robuckets_autonomous_regionals extends LinearOpMode {
         rightBack = hardwareMap.dcMotor.get("rightBack");
         leftBack = hardwareMap.dcMotor.get("leftBack");
 
-        Launch1 = hardwareMap.dcMotor.get("Launch1")
-        Launch2 = hardwareMap.dcMotor.get("Launch2")        
-        collector = hardwareMap.dcMotor.get("collector")
+        Launch1 = hardwareMap.dcMotor.get("Launch1");
+        Launch2 = hardwareMap.dcMotor.get("Launch2");
+        collector = hardwareMap.dcMotor.get("collector");
         launchServo = hardwareMap.servo.get("launchServo");
 
-        rightFront.setMode(DcMotor.RunMode.RUN_WITOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -46,39 +46,44 @@ public class robuckets_autonomous_regionals extends LinearOpMode {
         Launch2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         collector.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
-        color_sensor = hardwafeareMap.colorSensor.get("color");
+        color_sensor = hardwareMap.colorSensor.get("color");
 
         waitForStart();
-           
-        while (opModeIsActive() && counter != 1):
-            color_sensor.enableLed(false);
-            auto();
-            counter = 1;     
+
+        while (opModeIsActive() && active != 1):
+        color_sensor.enableLed(false);
+        auto();
+        active = 1;
 
     }
-    
+
     public void auto() {
-        strafeDistance(4 ft);
-        driveDistance(1.7 ft);
-        while (color_sensor.blue > 5 && color_sensor.blue < 10):
+        strafeDistance(1000); //4 ft
+        driveDistance(1000); //1.7 ft
+        while (color_sensor.blue() > 5 && color_sensor.blue() < 10) {
             strafe(0.5);
+        }
         stopDriving();
         delay(1.0);
-        if (color_sensor.blue > 5 && color_sensor.blue < 10):
-            driveDistance(0.25 ft);
+        if (color_sensor.blue() > 5 && color_sensor.blue() < 10) {
+            driveDistance(1000); //0.25 ft
             stopDriving();
-            driveDistance(-0.25 ft);
-            strafeDistance(0.5 ft);
-        while (color_sensor.blue > 5 && color_sensor.blue < 10):
+            driveDistance(1000); //-0.25 ft
+            strafeDistance(1000); //0.25 ft
+        }
+        while (color_sensor.blue() > 5 && color_sensor.blue() < 10) {
             strafe(0.5);
+        }
         stopDriving();
         delay(1.0);
-        if (color_sensor.blue > 5 && color_sensor.blue < 10):
-            driveDistance(0.25 ft);
+        if (color_sensor.blue() > 5 && color_sensor.blue() < 10) {
+            driveDistance(1000); //0.25 ft
             stopDriving();
-            driveDistance(-0.75 ft);
-        turnDistance(0.75 ft);
-        autoLaunch(0.5);
+            driveDistance(1000); //-0.75 ft
+        }
+        turnDistance(1000); //0.75 ft
+        autoLaunch(0.5, 0.75);
+    }
 
     public void delay(double secs) { /* creation of a wait method*/
         try {
@@ -92,21 +97,21 @@ public class robuckets_autonomous_regionals extends LinearOpMode {
         rightFront.setPower(-power);
         leftFront.setPower(power);
         rightBack.setPower(-power);
-        leftBack.setPower(power)
+        leftBack.setPower(power);
     }
 
     public void strafe(double power) {
         rightFront.setPower(power);
         leftFront.setPower(power);
         rightBack.setPower(-power);
-        leftBack.setPower(-power)
+        leftBack.setPower(-power);
     }
-    
+
     public void turn(double power) {
         rightFront.setPower(power);
         leftFront.setPower(power);
         rightBack.setPower(power);
-        leftBack.setPower(power)
+        leftBack.setPower(power);
     }
 
     public void launch(double launchPower) {
@@ -114,9 +119,9 @@ public class robuckets_autonomous_regionals extends LinearOpMode {
         Launch1.setPower(launchPower);
         Launch2.setPower(launchPower);
     }
-        
+
     public void stopDriving() {
-        driveForward(0);
+        drive(0);
         strafe(0);
         turn(0);
     }
@@ -124,7 +129,7 @@ public class robuckets_autonomous_regionals extends LinearOpMode {
     public void stopLaunch() {
         launch(0);
         collector.setPower(0);
-        launchServo.setPower(0);
+        launchServo.setPosition(0);
     }
 
     public void driveDistance(double distance) {
@@ -145,7 +150,7 @@ public class robuckets_autonomous_regionals extends LinearOpMode {
 
         leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
-    
+
     public void strafeDistance(double distance) {
 
         rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -183,7 +188,7 @@ public class robuckets_autonomous_regionals extends LinearOpMode {
 
         leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
-        
+
     public void autoLaunch(double launchPower, double collectPower) {
 
         launch(launchPower);
