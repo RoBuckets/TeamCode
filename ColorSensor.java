dffbf2b2f63a609fc7dbfdcf9cbb797306906504
RoBuckets <nmhsrobuckets@gmail.com>
2017-11-29 11:33:54 -0800

    Update ColorSensor.java

--- ColorSensor.java
+++ ColorSensor.java
@@ -1,3 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.ColorSensor;
+
+public class PracticeColorSensor extends OpMode {
+  
+  ColorSensor color_sensor;
+  
+  public void init() {
+    
+    color_sensor = hardwareMap.colorSensor.get(___) 
+  
+  // put the name of the sensor in the configuration file in the blank //
+      
+  }
+  
+}
+
